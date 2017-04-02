ab0a5a98d22eb46453ce9d150fb997c3354841b2
csubero <carlossuberoagreda@gmail.com>
2017-04-02 10:23:48 -0400

    Fixing comma

--- sportnutrition/settings/prod.py
+++ sportnutrition/settings/prod.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y)90@1lex*j%47*5*((ws1c8f)2zs3c&*1584x60!f)-ulh%tm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.sportnutritionlt.com', '52.25.244.21']
 
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'backend'
+	'backend',
 	'frontend',
 ]
 
