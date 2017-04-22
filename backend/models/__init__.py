6ea7c5eb9b58fd8ce79c1977ae07f16cda990136
csubero <carlossuberoagreda@gmail.com>
2017-04-22 17:40:33 -0400

    Created Gallery and Image model

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,3 +1,5 @@
 from .category import Category
 from .post_model import Post
 from .tip import Tip
+from .gallery import Gallery
+from .image import Image
--- backend/models/image.py
+++ backend/models/image.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+from backend.models.gallery import Gallery
+
 
 class Image(models.Model):
 	title = models.CharField(max_length=150, null=True, blank=True)
 	image = models.ImageField()
 	order = models.IntegerField(default=0)
 
+	gallery = models.ForeignKey(Gallery, default=None, null=True, related_name='images')
+
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title.title()
