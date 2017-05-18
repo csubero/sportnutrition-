bc9a56bf69d11219e94a88c7425a6f574cd4214e
csubero <carlossuberoagreda@gmail.com>
2017-05-18 10:09:49 -0400

    Setting layout

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 
-from frontend.views import IndexTemporalView
+from frontend.views import IndexTemporalView, IndexView
 
 urlpatterns = [
 	url(r'^$', IndexTemporalView.as_view(), name='frontend.index'),
+	url(r'^home/$', IndexView.as_view(), name='frontend.index.index'),
 ]
-
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/views/index_view.py
+++ frontend/views/index_view.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views import View
 
 
+class IndexView(View):
+	template_name = 'frontend/index/_index.html'
+
+	def get(self, request):
+		return render(request, self.template_name)
+
+
 class IndexTemporalView(View):
 	template_name = 'frontend/index/index.html'
 
