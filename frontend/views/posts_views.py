3253a6b4f5d787bcd0f82b8c5c3c43277785c2a0
csubero <carlossuberoagreda@gmail.com>
2017-06-11 19:36:51 -0400

    Format single post layout

--- frontend/views/posts_views.py
+++ frontend/views/posts_views.py
@@ -1,12 +1,18 @@
 from django.views.generic import DetailView
 
-from backend.models import Post
+from backend.models import Post, Category
 
 
 class PostDetailView(DetailView):
-
 	model = Post
 
 	template_name = 'frontend/post/post_single.html'
 
 	context_object_name = 'post'
+
+	def get_context_data(self, **kwargs):
+		data = super(PostDetailView, self).get_context_data(**kwargs)
+
+		data['categories'] = Category.objects.all().order_by('name')
+
+		return data
