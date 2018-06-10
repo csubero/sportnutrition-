29ca7b7aeea7a28f52a839c0787cbb92bc316a62
csubero <carlossuberoagreda@gmail.com>
2018-06-09 22:45:07 -0600

    Add domain

--- sportnutrition/settings/prod.py
+++ sportnutrition/settings/prod.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'y)90@1lex*j%47*5*((ws1c8f)2zs3c&*1584x60!f)-ulh%tm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.sportnutritionlt.com', '52.91.102.144']
+ALLOWED_HOSTS = ['.sportnutritionlt.com', '52.91.102.144', 'ec2-52-91-102-144.compute-1.amazonaws.com']
 
 # Application definition
 
